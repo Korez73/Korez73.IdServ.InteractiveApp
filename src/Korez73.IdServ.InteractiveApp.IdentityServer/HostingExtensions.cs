c2bce7d9c12550fd8ecccc67f70f403b13f78239
Jamie Weldon <jamie.weldon@outlook.com>
2023-11-12 06:23:20 -0600

    add google auth secrets not working yet

--- src/Korez73.IdServ.InteractiveApp.IdentityServer/HostingExtensions.cs
+++ src/Korez73.IdServ.InteractiveApp.IdentityServer/HostingExtensions.cs
@@ -50,8 +50,12 @@ internal static class HostingExtensions
                 // register your IdentityServer with Google at https://console.developers.google.com
                 // enable the Google+ API
                 // set the redirect URI to https://localhost:5001/signin-google
-                options.ClientId = "copy client ID from Google here";
-                options.ClientSecret = "copy client secret from Google here";
+                // options.ClientId = "copy client ID from Google here";
+                // options.ClientSecret = "copy client secret from Google here";
+
+                options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+                options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+
             });
 
         return builder.Build();
--- src/Korez73.IdServ.InteractiveApp.IdentityServer/Program.cs
+++ src/Korez73.IdServ.InteractiveApp.IdentityServer/Program.cs
@@ -11,6 +11,9 @@ try
 {
     var builder = WebApplication.CreateBuilder(args);
 
+    var movieApiKey = builder.Configuration["Authentication:Google:ClientId"];
+    
+
     builder.Host.UseSerilog((ctx, lc) => lc
         .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
         .Enrich.FromLogContext()
